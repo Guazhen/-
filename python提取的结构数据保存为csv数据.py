c1e6049e93ec45ce425757e93c8976c3780addfa
Guazhen <32102311+Guazhen@users.noreply.github.com>
2017-12-17 23:21:31 +0800

    Update python提取的结构数据保存为csv数据.py

--- python提取的结构数据保存为csv数据.py
+++ python提取的结构数据保存为csv数据.py
@@ -10,14 +10,18 @@ user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, lik
 headers = {'User_Agent':user_agent}
 r = requests.get('http://seputu.com/',headers = headers)
 
+#lxml解析html
 html = etree.HTML(r.text)
+#提取
 div_mulus = html.xpath('.//*[@class="mulu"]')
 pattern = re.compile(r'\s*\[(.*)\]\s+(.*)')
 rows = []
 for div_mulu in div_mulus:
+    #提取文本内容
     div_h2 = div_mulu.xpath('./div[@class="mulu-title"]/center/h2/text()')
     if len(div_h2) > 0:
         h2_title = div_h2[0].encode('utf-8')
+        #提取连接
         a_s = div_mulu.xpath('./div[@class="box"]/ul/li/a')
         for a in a_s:
             href = a.xpath('./@href')[0]
@@ -32,7 +36,10 @@ for div_mulu in div_mulus:
                 rows.append(content)
 headers = ['title','real_title','href','date']
 with open('qiye.csv','w') as f:
+    #返回writer对象f_csv
     f_csv = csv.writer(f)
+    #对句柄f_csv操作，写一行
     f_csv.writerow(headers)
+    #写入多行
     f_csv.writerows(rows)
 
